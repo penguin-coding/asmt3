a800d54de11b5b908363dc15d76fee0a559a3ac6
Cal-FJ-MT4113 <32537400+Cal-FJ-MT4113@users.noreply.github.com>
2017-11-16 15:58:01 +0000

    Pilot study stuff

--- num.of.simulations.choice.r
+++ num.of.simulations.choice.r
@@ -1,65 +1,26 @@
-source('simulation.r')
-dosims <- function(x,sample.n,boot.n){
-    
-    norm.sim <- simulation(dist.func=rnorm,
-                           simulations=x,
-                           sample.n=sample.n,
-                           boot.n=boot.n,
-                           boot.method=
-                             c('percentile','BCa','parametric','smooth'),
-                           stat.func=mean,
-                           smooth.sd=0.1)
+#### NEEDS TO BE RUN ON A CLEAN ENVIRONMENT AND ENVIRONMENT SHOULD BE CLEANED
+#### BEFORE RUNNING FULL SIMULATION
+
+### Note: running this code on any other machines will not produce the same
+### predicted times, refer to the figure in the report for the specific
+### graph this produced on my machine. 
+
+x <- seq(5)  # Setting this to 5 takes 2 minutes on my machine, but 3 is enough
+y <- rep(NA, length(x))
+
+for (j in x){
+  # Our x values are the number of different simulations we run for each case.
+  # Our y values is the computation time required for the respective x.
+  # We will attempt to draw a graph of simulations per case and computation 
+  # time, so that we can select a compromise between high simulations and 
+  # available time.
   
-    pois.sim <- simulation(dist.func=rpois,
-                           simulations=x,
-                           sample.n=sample.n,
-                           boot.n=boot.n,
-                           boot.method=
-                             c('percentile','BCa','parametric','smooth'),
-                           stat.func=mean,
-                           smooth.sd=0.1,
-                           lambda=100)
-  
-    gamm.sim <- simulation(dist.func=rgamma,
-                           simulations=x,
-                           sample.n=sample.n,
-                           boot.n=boot.n,
-                           boot.method=c('percentile','BCa','smooth'),
-                           stat.func=mean,
-                           smooth.sd=0.1,
-                           shape=3,
-                           rate=10)
-  
-  smooth.sd <- seq(0.01,0.25,length=10)
-  smoot.sim <- as.list(rep(NA, length(smooth.sd)))
-  
-  counter = 0
-  for (i in smooth.sd){
-    counter = counter + 1
-    sim <- simulation(dist.func=rnorm,
-                      simulations=x,
-                      sample.n=500,
-                      boot.n=999,
-                      boot.method='smooth',
-                      stat.func=mean,
-                      smooth.sd=smooth.sd[counter])
-    smoot.sim[[counter]] <- sim[1,1,1,]
-  }
-  
-  
-  return()
+  pilot.sims <- x[j]
+  y[j] <- system.time(source('driver.R'))[3]
 }
 
-get.time = function(x,sample.n,boot.n){
-  return(system.time(dosims(x,sample.n,boot.n))[3])
-}
-
-x <- seq(2)
-y <-  lapply(x, get.time, sample.n=c(50,100,500,1000), boot.n=c(99,199,499,999))
-y <- unlist(y)
-
-A = lm(y~x-1)  # We fit a lm with no intercept, and check
-summary(A)     # it produces a reasonable answer. 
+A = lm(y~x-1)    # We fit a lm and check it
+summary(A)       # produces a reasonable answer. 
 
 plot(x,y)                    # We visualise the fitted line
 abline(0,A$coefficients)
@@ -77,4 +38,4 @@ plot.y = pred.time(plot.x)  # so that we can choose how many to use
 plot(plot.x, plot.y, xlab = 'number of simulations per case',
      ylab = 'Time for all simulations to run - hours',
      type='l',
-     main = 'Predicted calculation times for 1.51GHz CPU')
+     main = 'Predicted calculation times for 4.0GHz CPU')
