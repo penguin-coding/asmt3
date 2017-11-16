1f3201e78711aea6fea32faa324878f823327394
Cal-FJ-MT4113 <32537400+Cal-FJ-MT4113@users.noreply.github.com>
2017-11-16 15:57:46 +0000

    More comments added

--- driver.R
+++ driver.R
@@ -8,16 +8,25 @@ set.seed(12**3*4**5)  # to make results reproducible to the reader
 
 ##### WARNING #####
 
-# This simulation takes a long time to run - roughly 3 hours on a 4.0 GHz
+# This simulation takes a long time to run - roughly 2 hours on a 4.0 GHz
 # processor. The global environment produced by this file is made
 # available in the project zip file as 'SimGlobalEnv.r'.
 
-print(Sys.time()) # We also print the end time
+Sys.time() %>% paste('was our start time') %>% print() # print the start time
 
 sample.n <- c(50,100,500,1000)
 boot.n <- c(99,199,499,999)
-simulations <- 1000
 boot.method=c('percentile','BCa','parametric','smooth','par.fit')
+
+# The pilot study function goes through this code and times how long the file
+# takes to run for various values of 'simulations'. In order to do this, the 
+# pilot file will set a variable pilot.sims, we check if this variable exists
+# (which indicates that this file is being sourced from the pilot file), and if
+# it does we use the hardcoded value, otherwise, we use the one specified 
+# by the pilot file
+ifelse(exists('pilot.sims'), simulations <- pilot.sims, simulations <- 1000)
+
+
 # Full simulation with normal data
 norm.sim <- simulation(dist.func='rnorm',
                        simulations=simulations,
@@ -78,7 +87,8 @@ for (i in smooth.sd){
   smoot.sim[[counter]] <- sim[1,1,1,]
 }
 remove(i) ; remove(counter) # remove unnecessary global vars
-print(Sys.time())
+
+Sys.time() %>% paste('was our end time') %>% print() # print the end time
 
 
 
@@ -86,79 +96,97 @@ print(Sys.time())
 
 ### side analysis looking at BCa vs percentile for low n and high resamples ####
 
+
+set.seed(123454321) # This simulation was added after the main simulations were
+# done, so we set the seed again to ensure the below simulations always return
+# the same results
+
 pois.sim2 <- simulation(dist.func='rpois',
-                        simulations=simulations,
-                        sample.n=c(10,20,35,50),
+                        simulations=5*simulations,
+                        sample.n=c(20,35,50),
                         boot.n=c(2999,3499),
                         boot.method=c('percentile','BCa'),
                         stat.func=mean,
                         lambda=100)
 
 
+gamm.sim2 <- simulation(dist.func='rgamma',
+                        simulations=5*simulations,
+                        sample.n=c(20,35,50),
+                        boot.n=c(2999,3499),
+                        boot.method=c('percentile','BCa'),
+                        stat.func=mean,
+                        shape=3,
+                        rate=10)
 
 
+if (!exists('pilot.sims')){
+############## Producing plots to visualise the simulation results #############
 
-############# Producing plots to visualise the simulation results ##############
+  norm.results <- calculate.summaries(norm.sim, 0)    # calculate coverage, len
+  pois.results <- calculate.summaries(pois.sim, 100)  # and 'failure tendency'
+  gamm.results <- calculate.summaries(gamm.sim, 3/10) # for each simulation
 
-norm.results <- calculate.summaries(norm.sim, 0)    # calculate coverage, length
-pois.results <- calculate.summaries(pois.sim, 100)  # and 'failure tendency'
-gamm.results <- calculate.summaries(gamm.sim, 3/10) # for each simulation
-
-for (results in list(norm.results, pois.results, gamm.results)){
+  for (results in list(norm.results, pois.results, gamm.results)){
   
-  for (statistic in c('coverage','length','failure tendency')){
+    for (statistic in c('coverage','length','failure tendency')){
     
-    # to avoid "the condition has length > 1" warnings, we compare the first
-    # element only - which should be sufficient for our purposes:
-    if (results[1,1,1,1]==norm.results[1,1,1,1]){main <- 'normal'}
-    else if (results[1,1,1,1]==pois.results[1,1,1,1]){main <- 'poisson'}
-    else {main <- 'gamma'}
+      # to avoid "the condition has length > 1" warnings, we compare the first
+      # element only - which should be sufficient for our purposes:
+      if (results[1,1,1,1]==norm.results[1,1,1,1]){main <- 'normal'}
+      else if (results[1,1,1,1]==pois.results[1,1,1,1]){main <- 'poisson'}
+      else {main <- 'gamma'}
     
-    main <- paste(main, 'simulated deviates')
+      main <- paste(main, 'simulated deviates')
     
-    plot(results, statistic=statistic, main=main)
+      plot(results, statistic=statistic, main=main)
+    }
   }
-}
 
-smooth.stats <- array(dim=c(length(smoot.sim),3))
+  smooth.stats <- array(dim=c(length(smoot.sim),3))
 
-for (i in 1:length(smoot.sim)){                         # we manually create a
-  cov <- get.coverage(smoot.sim[[i]],0)                 # a matrix of summary 
-  len <- get.length(smoot.sim[[i]])                     # statistics for the
-  f.t <- get.coverage(smoot.sim[[i]],0,failure.t = T)   # simulation where only
-  smooth.stats[i,] <- c(cov,len,f.t)                    # the smooth.sd
-}                                                       # quantity varies
-
-
-# We manually produce some plots for the smooth bootstrap analysis:
-for (i in 1:3){
-  ylab <-  switch(i, '1'='coverage', '2'='length', '3'='failure tendency')
-  main <- 'Smooth bootstrap'
-  y <- smooth.stats[,i]
-  plot(smooth.sd, y,xlab = 'smooth.sd', ylab=ylab, main=main,
-       col=4)
-}
+  for (i in 1:length(smoot.sim)){                       # we manually create a
+    cov <- get.coverage(smoot.sim[[i]],0)               # a matrix of summary 
+    len <- get.length(smoot.sim[[i]])                   # statistics for the
+    f.t <- get.coverage(smoot.sim[[i]],0,failure.t = T) # simulation where only
+    smooth.stats[i,] <- c(cov,len,f.t)                  # the smooth.sd
+  }                                                     # quantity varies
 
 
-# Produce the plot which supports our claim that the BCa is better for low n:
-pois.sim2 %>% calculate.summaries(100) %>% plot(statistic='coverage',
-                                                main='Poisson data')
+  # We manually produce some plots for the smooth bootstrap analysis:
+  for (i in 1:3){
+    ylab <-  switch(i, '1'='coverage', '2'='length', '3'='failure tendency')
+    main <- 'Smooth bootstrap'
+    y <- smooth.stats[,i]
+    plot(smooth.sd, y,xlab = 'smooth.sd', ylab=ylab, main=main,
+         col=4)
+  }
 
 
+  for (statistic in c('coverage','length','failure tendency')){
+    # Produce the plot which supports our claim that the BCa is better for low n:
+    pois.sim2 %>% calculate.summaries(100) %>% plot(statistic=statistic,
+                                                    main='Poisson data')
+  
+    gamm.sim2 %>% calculate.summaries(3/10) %>% plot(statistic=statistic,
+                                                     main='Gamma data')
 
+  }
 
 
 ################ Create some 3D plots for the standard analyses ################
 
-for (method in 1:4){
-  for (statistic in c('coverage')){
+  for (method in 1:4){
+    for (statistic in c('coverage')){
 
-    # Because parametric bootstraps have a coverage of 1 in these simulations,
-    # the failure tendency is NaN, since there are no failures. Hence we skip
-    # this case to avoid errors with plot.sim.3D:
-    if (statistic!='length' & method==3) next
+      # Because parametric bootstraps have a coverage of 1 in these simulations,
+      # the failure tendency is NaN, since there are no failures. Hence we skip
+      # this case to avoid errors with plot.sim.3D:
+      if (statistic!='length' & method==3) next
 
-    sim.plot.3D(norm.results, statistic=statistic, method=method, hist=F)
+      sim.plot.3D(norm.results, statistic=statistic, method=method, hist=F)
 
+    }
   }
+
 }
